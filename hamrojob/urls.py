e1bbe3872d17f4ed9959aa7a1b1757557fa76927
Ayub Shrestha <ayubsht1@gmail.com>
2024-10-18 12:09:49 +0545

    minor tweks

--- hamrojob/urls.py
+++ hamrojob/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, JobView, JobDetailView, ContactView
 urlpatterns = [
     path('',HomeView.as_view(),name='home'),
+    path('job-list',JobView.as_view(),name='job-list'),
+    path('job-detail',JobDetailView.as_view(),name='job-detail'),
+    path('contact',ContactView.as_view(),name='contact'),
 ]
--- hamrojob/views.py
+++ hamrojob/views.py
@@ -4,3 +4,12 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+
+class JobView(TemplateView):
+    template_name = 'job/job_list.html'
+
+class JobDetailView(TemplateView):
+    template_name = 'job/job_detail.html'
+
+class ContactView(TemplateView):
+    template_name = 'contact.html'

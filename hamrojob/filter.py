adb48b5077e3b3852a848a6f8bfefce08aec693a
Ayub Shrestha <ayubsht1@gmail.com>
2024-11-13 22:40:15 +0545

    working filter

--- hamrojob/filter.py
+++ hamrojob/filter.py
@@ -1,7 +1,31 @@
 import django_filters
-from . models import Job
+from . models import Job,JobCategory
+from django import forms
 
 class Jobfilter(django_filters.FilterSet):
     class Meta:
         model = Job
         fields = ['title', 'job_type', 'job_type', 'is_available' ]
+
+class Jobfilter(django_filters.FilterSet):
+    # Job category as checkboxes
+    category = django_filters.ModelMultipleChoiceFilter(
+        queryset=JobCategory.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        label="Job Category"
+    )
+    
+    # Job type as checkboxes
+    job_type = django_filters.MultipleChoiceFilter(
+        choices=[
+            ('Full Time', 'Full Time'),
+            ('Part Time', 'Part Time'),
+            ('Remote', 'Remote'),
+            ('Freelance', 'Freelance')
+        ],
+        widget=forms.CheckboxSelectMultiple
+    )
+
+    class Meta:
+        model = Job
+        fields = ['category', 'job_type']
